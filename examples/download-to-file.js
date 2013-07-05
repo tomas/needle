7251e84a4b99eab394ea140a1ebf182f186e0907
Tomás Pollak <tomaspollak@gmail.com>
2013-07-04 22:04:22 -0400

    Return readable Stream so requests can be piped out.

--- examples/download-to-file.js
+++ examples/download-to-file.js
@@ -1,10 +1,13 @@
 var fs = require('fs'),
-    needle = require('./..');
+    needle = require('./..'),
+    path = require('path');
 
-var file = 'tux.png',
-    url  = 'http://upload.wikimedia.org/wikipedia/commons/a/af/Tux.png';
+var url  = process.argv[2] || 'https://upload.wikimedia.org/wikipedia/commons/a/af/Tux.png';
+var file = path.basename(url);
 
-needle.get(url, { output: file }, function(err, resp, data){
+console.log('Downloading ' + file);
+
+needle.get(url, { output: file, follow: true }, function(err, resp, data){
   console.log('File saved: ' + process.cwd() + '/' + file);
   console.log(resp.bytes + ' bytes transferred.');
 });
