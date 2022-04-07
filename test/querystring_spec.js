730ca5702750317116a432bffe918704e059c946
Tomás Pollak <tomaspollak@gmail.com>
2022-04-07 12:07:07 -0400

    WIP: Add array item index when building querystring (for complex objects) #400

--- test/querystring_spec.js
+++ test/querystring_spec.js
@@ -109,7 +109,7 @@ describe('stringify', function() {
 
 				it('works', function() {
 					var res = stringify({ foo: ['bar', 'baz'] });
-					res.should.eql('foo[]=bar&foo[]=baz');
+					res.should.eql('foo[0]=bar&foo[1]=baz');
 				})
 
 			})
@@ -118,11 +118,48 @@ describe('stringify', function() {
 
 				it('works', function() {
 					var res = stringify({ foo: [{'1': 'bar'}, {'2': 'baz'}] });
-					res.should.eql('foo[][1]=bar&foo[][2]=baz');
+					res.should.eql('foo[0][1]=bar&foo[1][2]=baz');
 				})
 
 			})
 
+	})
+
+	describe('with a complex object', function() {
+
+		var data = {
+			field1: 'x',
+			field3: [
+				{
+					subfield1: 'this is a string',
+					subfield2: ['this is array'],
+					subfield3: ['another array']
+				},
+				{
+					subfield1: 'this is a string',
+					subfield2: ['this is array'],
+					subfield3: ['another array']
+				},
+				{
+					subfield1: 'this is a string',
+					subfield2: ['this is array'],
+					subfield3: ['another array']
+				},
+			]
+		};
+
+		// TODO
+		it('works as expected', function() {
+			var encoded = stringify(data)
+			encoded.should.eql('field1=x&field3[0][subfield1]=this%20is%20a%20string&field3[0][subfield2][0]=this%20is%20array&field3[0][subfield3][0]=another%20array&field3[1][subfield1]=this%20is%20a%20string&field3[1][subfield2][0]=this%20is%20array&field3[1][subfield3][0]=another%20array&field3[2][subfield1]=this%20is%20a%20string&field3[2][subfield2][0]=this%20is%20array&field3[2][subfield3][0]=another%20array')
+
+			// console.log(data);
+			// console.log(encoded.replace(/\&/g, '\n'));
+
+			// var qs = require("querystring");
+			// var res = qs.decode(encoded);
+			// console.log(res);
+		})
 	})
 
 })
